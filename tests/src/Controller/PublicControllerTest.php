b9c2008761e4afce34b9511fa197fb3ea48dea36
AntoineSmagghe <antoine@smagghe.me>
2020-03-01 22:50:51 +0100

    add tests

---
+++ tests/src/Controller/PublicControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests\App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PublicControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
